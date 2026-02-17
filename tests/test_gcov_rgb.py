c8d28b1208049f236b3dcd0536a33e79c88b32c3
Jake Herrmann <jtherrmann@alaska.edu>
2026-02-17 11:21:56 -0900

    add test_gcov_rgb

---
+++ tests/test_gcov_rgb.py
@@ -0,0 +1,16 @@
+from nisar_py import gcov_rgb
+
+def test_get_polarization_names():
+    assert gcov_rgb._get_polarization_names(['HH', 'HV']) == ('HHHH', 'HVHV')
+    assert gcov_rgb._get_polarization_names(['HV', 'HH']) == ('HHHH', 'HVHV')
+
+    assert gcov_rgb._get_polarization_names(['HH']) is None
+    assert gcov_rgb._get_polarization_names(['HV']) is None
+
+    assert gcov_rgb._get_polarization_names(['VV', 'VH']) == ('VVVV', 'VHVH')
+    assert gcov_rgb._get_polarization_names(['VH', 'VV']) == ('VVVV', 'VHVH')
+
+    assert gcov_rgb._get_polarization_names(['VV']) is None
+    assert gcov_rgb._get_polarization_names(['VH']) is None
+
+    assert gcov_rgb._get_polarization_names([]) is None
