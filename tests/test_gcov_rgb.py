03be0045c626079416cb75876878a321c07c9e39
Jake Herrmann <jtherrmann@alaska.edu>
2026-02-17 11:25:28 -0900

    ruff

--- tests/test_gcov_rgb.py
+++ tests/test_gcov_rgb.py
@@ -1,5 +1,6 @@
 from nisar_py import gcov_rgb
 
+
 def test_get_polarization_names():
     assert gcov_rgb._get_polarization_names(['HH', 'HV']) == ('HHHH', 'HVHV')
     assert gcov_rgb._get_polarization_names(['HV', 'HH']) == ('HHHH', 'HVHV')
