25f2fd322bda5c19d3d35ee2617a05312c333ff6
jacquelynsmale <jacquelyn.smale@gmail.com>
2026-05-05 10:04:56 -0800

    update for single pol

--- tests/test_gcov_rgb.py
+++ tests/test_gcov_rgb.py
@@ -5,13 +5,13 @@ def test_get_polarization_names():
     assert gcov_rgb._get_polarization_names(['HH', 'HV']) == ('HHHH', 'HVHV')
     assert gcov_rgb._get_polarization_names(['HV', 'HH']) == ('HHHH', 'HVHV')
 
-    assert gcov_rgb._get_polarization_names(['HH']) is None
-    assert gcov_rgb._get_polarization_names(['HV']) is None
+    assert gcov_rgb._get_polarization_names(['HH']) == ('HHHH', None)
+    assert gcov_rgb._get_polarization_names(['HV']) == (None, None)
 
     assert gcov_rgb._get_polarization_names(['VV', 'VH']) == ('VVVV', 'VHVH')
     assert gcov_rgb._get_polarization_names(['VH', 'VV']) == ('VVVV', 'VHVH')
 
-    assert gcov_rgb._get_polarization_names(['VV']) is None
-    assert gcov_rgb._get_polarization_names(['VH']) is None
+    assert gcov_rgb._get_polarization_names(['VV']) == ('VVVV', None)
+    assert gcov_rgb._get_polarization_names(['VH']) == (None, None)
 
-    assert gcov_rgb._get_polarization_names([]) is None
+    assert gcov_rgb._get_polarization_names([]) == (None, None)
