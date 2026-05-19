736fa5b40e90db0386ca5d9ab6ec63957d0bca92
Andrew Johnston <asjohnston@alaska.edu>
2026-05-18 16:17:21 -0800

    ruff formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,62 +24,66 @@ def mock_gcov_granule():
     x_cooridnates = [ii * step_size for ii in range(0, 1000, 1)]
     y_coorindates = [ii * step_size for ii in range(512, 0, -1)]
 
-    dt = DataTree.from_dict({
-        '/science/LSAR/GCOV/grids/frequencyA': Dataset(
-            {
-                'HHHH': Variable(
-                    dims=('yCoordinates', 'xCoordinates'),
-                    data=[[x for x in x_cooridnates] for y in y_coorindates],
-                ).astype('float32'),
-                'xCoordinates': x_cooridnates,
-                'yCoordinates': y_coorindates,
-                'xCoordinateSpacing': step_size,
-                'yCoordinateSpacing': -step_size,
-                'listOfPolarizations': (
-                    'phony_dim_0',
-                    ['HH'],
-                ),
-                'projection': DataArray(3413),
-            },
-        ),
-        '/science/LSAR/GCOV/grids/frequencyB': Dataset(
-            {
-                'VVVV': Variable(
-                    dims=('yCoordinates', 'xCoordinates'),
-                    data=[[x for x in x_cooridnates] for y in y_coorindates],
-                ).astype('float32'),
-                'VHVH': Variable(
-                    dims=('yCoordinates', 'xCoordinates'),
-                    data=[[y for x in x_cooridnates] for y in y_coorindates],
-                ).astype('float32'),
-                'xCoordinates': x_cooridnates,
-                'yCoordinates': y_coorindates,
-                'xCoordinateSpacing': step_size,
-                'yCoordinateSpacing': -step_size,
-                'listOfPolarizations': (
-                    'phony_dim_0',
-                    ['VV', 'VH'],
-                ),
-                'projection': DataArray(3413),
-            },
-        ),
-        '/science/LSAR/identification': Dataset({
-            'productType': 'GCOV',
-            'missionId': 'NISAR',
-            'absoluteOrbitNumber': 123,
-            'lookDirection': 'Left',
-            'orbitPassDirection': 'Ascending',
-            'zeroDopplerStartTime': '2026-01-01T00:00:00.000000000',
-            'zeroDopplerEndTime': '2026-01-01T00:00:01.000000000',
-            'boundingPolygon': 'POLYGON ((0.0 0.0, 0.0 .512, 1.0 .512, 1.0 0.0, 0.0 0.0))',
-            'listOfFrequencies': ['A', 'B'],
-            'diagnosticModeFlag': 0,
-            'plannedDatatakeId': 'dtid_2025359150444',
-            'plannedObservationId': 'oid_2025359150500',
-            'isUrgentObservation': 'False',
-            'isJointObservation': 'False',
-        }),
-    })
+    dt = DataTree.from_dict(
+        {
+            '/science/LSAR/GCOV/grids/frequencyA': Dataset(
+                {
+                    'HHHH': Variable(
+                        dims=('yCoordinates', 'xCoordinates'),
+                        data=[[x for x in x_cooridnates] for y in y_coorindates],
+                    ).astype('float32'),
+                    'xCoordinates': x_cooridnates,
+                    'yCoordinates': y_coorindates,
+                    'xCoordinateSpacing': step_size,
+                    'yCoordinateSpacing': -step_size,
+                    'listOfPolarizations': (
+                        'phony_dim_0',
+                        ['HH'],
+                    ),
+                    'projection': DataArray(3413),
+                },
+            ),
+            '/science/LSAR/GCOV/grids/frequencyB': Dataset(
+                {
+                    'VVVV': Variable(
+                        dims=('yCoordinates', 'xCoordinates'),
+                        data=[[x for x in x_cooridnates] for y in y_coorindates],
+                    ).astype('float32'),
+                    'VHVH': Variable(
+                        dims=('yCoordinates', 'xCoordinates'),
+                        data=[[y for x in x_cooridnates] for y in y_coorindates],
+                    ).astype('float32'),
+                    'xCoordinates': x_cooridnates,
+                    'yCoordinates': y_coorindates,
+                    'xCoordinateSpacing': step_size,
+                    'yCoordinateSpacing': -step_size,
+                    'listOfPolarizations': (
+                        'phony_dim_0',
+                        ['VV', 'VH'],
+                    ),
+                    'projection': DataArray(3413),
+                },
+            ),
+            '/science/LSAR/identification': Dataset(
+                {
+                    'productType': 'GCOV',
+                    'missionId': 'NISAR',
+                    'absoluteOrbitNumber': 123,
+                    'lookDirection': 'Left',
+                    'orbitPassDirection': 'Ascending',
+                    'zeroDopplerStartTime': '2026-01-01T00:00:00.000000000',
+                    'zeroDopplerEndTime': '2026-01-01T00:00:01.000000000',
+                    'boundingPolygon': 'POLYGON ((0.0 0.0, 0.0 .512, 1.0 .512, 1.0 0.0, 0.0 0.0))',
+                    'listOfFrequencies': ['A', 'B'],
+                    'diagnosticModeFlag': 0,
+                    'plannedDatatakeId': 'dtid_2025359150444',
+                    'plannedObservationId': 'oid_2025359150500',
+                    'isUrgentObservation': 'False',
+                    'isJointObservation': 'False',
+                },
+            ),
+        },
+    )
 
     with NamedTemporaryFile() as temp_file:
         dt.to_netcdf(
@@ -92,6 +96,6 @@ def mock_gcov_granule():
                     'VVVV': {'chunksizes': (512, 512), 'compression': 'gzip'},
                     'VHVH': {'chunksizes': (512, 512), 'compression': 'gzip'},
                 },
-            }
+            },
         )
         yield Path(temp_file.name)
