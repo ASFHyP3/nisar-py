6d51cd3f7b416d84f4fb87cab0e9d0821156daae
Andrew Johnston <asjohnston@alaska.edu>
2026-05-19 16:02:34 -0800

    drop numpy for xarray + dask

--- src/nisar_py/gcov_rgb.py
+++ src/nisar_py/gcov_rgb.py
@@ -1,9 +1,9 @@
 """Create RGB image from GCOV product."""
 
 import argparse
+from math import pi
 from pathlib import Path
 
-import numpy as np
 import xarray as xr
 from osgeo import gdal, osr
 
@@ -17,36 +17,41 @@ class RGBDecompException(Exception):
     pass
 
 
-def _prepare_geotif_data(data: np.ndarray) -> np.ndarray:
-    data = np.nan_to_num(data, copy=False)
+def _prepare_geotif_data(data: xr.Dataset) -> xr.Dataset:
+    data = data.fillna(0.0)
     data[data < pow(10.0, -48.0 / 10.0)] = 0.0
     return data
 
 
 def _calculate_color_channel(
-    copol: np.ndarray, crosspol: np.ndarray, color: str, threshold: float = -24, scale_factor: float = 254.0
-) -> np.ndarray:
+    copol: xr.Dataset, crosspol: xr.Dataset | None, color: str, threshold: float = -24, scale_factor: float = 254.0
+) -> xr.Dataset:
+
+    if crosspol is None:
+        crosspol = copol.where(False, copol * 0.1)
+        crosspol = crosspol.where(copol > 0.4, copol * 0.0555555556 + 0.0177777778)
+        crosspol = crosspol.where(copol > 0.04, 0.0)
+
     power_threshold = 10.0 ** (threshold / 10.0)
     below_threshold_mask = crosspol < power_threshold
 
-    zp = np.arctan(np.sqrt(np.clip(copol - crosspol, 0, None))) * (2.0 / np.pi)
-    zp[~below_threshold_mask] = 0.0
+    zp = xr.ufuncs.arctan(xr.ufuncs.sqrt((copol - crosspol).clip(0))) * (2.0 / pi)
+    zp = zp.where(below_threshold_mask, 0.0)
 
     if color == 'red':
-        channel = 2.0 * np.sqrt(np.clip(copol - 3.0 * crosspol, 0, None))
-        channel[below_threshold_mask] = 0.0
+        channel = 2.0 * xr.ufuncs.sqrt((copol - 3.0 * crosspol).clip(0))
+        channel = channel.where(~below_threshold_mask, 0.0)
         channel += zp
     elif color == 'green':
-        channel = 3.0 * np.sqrt(crosspol)
-        channel[below_threshold_mask] = 0.0
+        channel = 3.0 * xr.ufuncs.sqrt(crosspol)
+        channel = channel.where(~below_threshold_mask, 0.0)
         channel += 2.0 * zp
     elif color == 'blue':
         channel = 5.0 * zp
 
     channel = channel * scale_factor + 1.0
 
-    invalid_copol_mask = ~(copol > 0)
-    channel[invalid_copol_mask] = 0.0
+    channel = channel.where(copol > 0, 0.0)
 
     return channel
 
@@ -69,7 +74,7 @@ def _get_polarization_names(pols: list[str]) -> tuple[str | None, str | None]:
 
 def make_rgb_geotiff(gcov_product: Path, output_path: Path, frequency: str | None = None) -> Path:
     """Create RGB GeoTIFF from GCOV product."""
-    gcov = xr.open_datatree(gcov_product)
+    gcov = xr.open_datatree(gcov_product, chunks='auto')
     grids = gcov.science.LSAR.GCOV.grids
 
     if frequency is None:
@@ -118,19 +123,11 @@ def make_rgb_geotiff(gcov_product: Path, output_path: Path, frequency: str | Non
     srs.ImportFromEPSG(int(frequency_group.projection.values))
     raster.SetProjection(srs.ExportToWkt())
 
-    copol_chunk = _prepare_geotif_data(copol_ds)
-    if crosspol_ds is not None:
-        crosspol_chunk = _prepare_geotif_data(crosspol_ds)
-    else:
-        crosspol_chunk = copol_chunk * 0.1
-        crosspol_chunk[copol_chunk <= 0.4] = copol_chunk[copol_chunk <= 0.4] * 0.0555555556 + 0.0177777778
-        crosspol_chunk[copol_chunk <= 0.04] = 0
-
     for band_idx, color in enumerate(('red', 'green', 'blue'), start=1):
-        channel = _calculate_color_channel(copol_chunk, crosspol_chunk, color)
+        channel = _calculate_color_channel(copol_ds, crosspol_ds, color)
 
         band = raster.GetRasterBand(band_idx)
-        band.WriteArray(channel)
+        band.WriteArray(channel.values)
         band.SetNoDataValue(0)
 
     # write RGB raster to disk as a cloud optimized geotiff
