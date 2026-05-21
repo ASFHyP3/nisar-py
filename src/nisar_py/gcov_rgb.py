48395e7116a5513ab23b6d5bbb3e40388b2fd2c8
Andrew Johnston <asjohnston@alaska.edu>
2026-05-21 13:39:50 -0800

    finalize mask implemenation

--- src/nisar_py/gcov_rgb.py
+++ src/nisar_py/gcov_rgb.py
@@ -101,18 +101,18 @@ def make_rgb_geotiff(gcov_product: Path, output_path: Path, frequency: str | Non
         y_slice, x_slice = chunk
         y_off, x_off = y_slice.start, x_slice.start
 
-        mask_chunk = np.isin(mask[chunk], [0, 255])
-
         copol_chunk = copol_ds[chunk]
-        copol_chunk[mask_chunk] = np.nan
 
         if crosspol_ds:
             crosspol_chunk = crosspol_ds[chunk]
-            crosspol_chunk[mask_chunk] = np.nan
         else:
             crosspol_chunk = copol_chunk * 0.1
             crosspol_chunk[copol_chunk <= 0.4] = copol_chunk[copol_chunk <= 0.4] * 0.0555555556 + 0.0177777778
             crosspol_chunk[copol_chunk <= 0.04] = 0
+
+        mask_chunk = np.isin(mask[chunk], [0, 255])
+        copol_chunk[mask_chunk] = np.nan
+        crosspol_chunk[mask_chunk] = np.nan
 
         for band_idx, color in enumerate(('red', 'green', 'blue'), start=1):
             channel = _calculate_color_channel(copol_chunk, crosspol_chunk, color)
